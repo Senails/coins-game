361240ec67e84b062840a60f2989c0a0e5df6f14
Senails <seffele@gmail.com>
2023-02-22 01:05:43 +0300

    add utils functions for UI

--- CoinsProject/Assets/Components/CoinCounter/CoinCounter.cs
+++ CoinsProject/Assets/Components/CoinCounter/CoinCounter.cs
@@ -7,12 +7,19 @@ public class CoinCounter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        placeOnStart();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void placeOnStart(){
+        Utils.setPositionRight(transform,20);
+        Utils.setPositionTop(transform,20);
+
+        Utils.showComponent(transform);
     }
 }
