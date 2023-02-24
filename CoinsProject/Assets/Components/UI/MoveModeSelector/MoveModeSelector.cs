233075392da5736bc7677e179e5fed6255ddb8ff
Senails <seffele@gmail.com>
2023-02-24 20:03:05 +0300

    work on first screen UI

--- CoinsProject/Assets/Components/UI/MoveModeSelector/MoveModeSelector.cs
+++ CoinsProject/Assets/Components/UI/MoveModeSelector/MoveModeSelector.cs
@@ -1,20 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveModeSelector : MonoBehaviour
 {
-    
+    static MoveModeSelector self;
 
-    // Start is called before the first frame update
     void Start()
     {
+        MoveModeSelector.self = this;
+        selectFromMemory();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    static public void Select(Transform obj){
+        for (int i = 0; i < self.transform.childCount; i++){
+            var child = self.transform.GetChild(i);
+
+
+            if (child == obj){
+                var sr = child.GetComponent<Image>();
+                sr.color = new Color(1, 1, 1, 1);
+                self.SetInMemory(i);
+            }else{
+                var sr = child.GetComponent<Image>();
+                sr.color = new Color(1, 1, 1, 0.5f);
+            }
+
+            Debug.Log(child == obj.transform);
+        }
+    }
+
+    void selectFromMemory(){
+        int index = this.GetFromMemory();
+
+        var child = transform.GetChild(index);
+        MoveModeSelector.Select(child);
+    }
+
+    void SetInMemory(int index){
+        Debug.Log(index);
+    }
+
+    int GetFromMemory(){
+        return 0;
     }
 }
