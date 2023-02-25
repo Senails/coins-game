4ce298cc92a0ea93054bc62d76723d996aef005c
Senails <seffele@gmail.com>
2023-02-25 22:59:22 +0300

    add visual for user

--- CoinsProject/Assets/Components/Game/User/UserImageController.cs
+++ CoinsProject/Assets/Components/Game/User/UserImageController.cs
@@ -7,14 +7,41 @@ public class UserImageController : MonoBehaviour
     int activeImage = 0;
     void Update()
     {
-        
+        int imageIndex = findActiveImage();
+        if (imageIndex!=activeImage){
+            activeNeedImage(imageIndex);
+        }
     }
 
     int findActiveImage(){
-        
+        float cosX = MoveController.cosX;
+        float sinY = MoveController.sinY;
+
+        if (cosX==0 && sinY==0){
+            return 0;
+        }
+
+        if (Mathf.Abs(sinY) > Mathf.Abs(cosX)){
+            return (sinY>0)?1:0;
+        }else{
+            return (cosX>0)?3:2;
+        }
+    }
+
+    void activeNeedImage(int index){
+        int childCount = transform.childCount;
 
 
+        for(int i=0; i<childCount;i++){
+            // Debug.Log(transform.GetChild(i));
+            var child = transform.GetChild(i);
+            child.gameObject.SetActive(false);
+        }
 
-        return 0;
+        var needChild = transform.GetChild(index);
+        needChild.gameObject.SetActive(true);
+
+        activeImage=index;
+        // Debug.Log(childCount);
     }
 }
