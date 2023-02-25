244217b2cff320cabe2461cb02914c5fd12460e5
Senails <seffele@gmail.com>
2023-02-25 19:10:59 +0300

    ended coinSpawner

--- CoinsProject/Assets/Components/Game/CoinSpawner/CoinSpawner.cs
+++ CoinsProject/Assets/Components/Game/CoinSpawner/CoinSpawner.cs
@@ -5,23 +5,16 @@ using UnityEngine;
 public class CoinSpawner : MonoBehaviour
 {
     public GameObject GameMap; 
+    public GameObject SmallCoinPrefab;
+    public GameObject LargeCoinPrefab;
+
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
     void Start()
     {
-        float x=0;
-        float y=0;
-
-        findPositionForSpawn(ref x, ref y);
-        Debug.Log($"{x} {y}");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("12312");
+        spawnCoins();
     }
 
     bool checkPosition(float x, float y){
@@ -32,7 +25,52 @@ public class CoinSpawner : MonoBehaviour
     }
 
     void findPositionForSpawn(ref float x, ref float y){
-        x=1;
-        y=4;
+        while (true){
+            float randX = Random.Range(minX,maxX);
+            float randY = Random.Range(minY,maxY);
+
+            if (checkPosition(randX,randY)){
+                x=randX;
+                y=randY;
+                break;
+            }
+        }
+    }
+
+    void findMinMaxPosition(){
+        float mapWidth = GameMap.transform.localScale.x;
+        float mapHeight = GameMap.transform.localScale.y;
+
+        float mapX = GameMap.transform.position.x;
+        float mapY = GameMap.transform.position.y;
+
+        this.minX = mapX-mapWidth/2;
+        this.maxX = mapX+mapWidth/2;
+        this.minY = mapY-mapHeight/2;
+        this.maxY = mapY+mapHeight/2;
+    }
+
+    void spawnCoins(){
+        findMinMaxPosition();
+
+        float x=0;
+        float y=0;
+
+        for(int i=0;i<50;i++){
+            findPositionForSpawn(ref x,ref y);
+            spawnOneCoin(x,y,"small");
+        }
+        for(int i=0;i<50;i++){
+            findPositionForSpawn(ref x,ref y);
+            spawnOneCoin(x,y,"large");
+        }
+    }
+
+    void spawnOneCoin(float x , float y, string size){
+        if (size=="small"){
+            Instantiate(SmallCoinPrefab,new Vector2(x,y),new Quaternion());
+        }else{
+            Instantiate(LargeCoinPrefab,new Vector2(x,y),new Quaternion());
+        }
     }
 }
