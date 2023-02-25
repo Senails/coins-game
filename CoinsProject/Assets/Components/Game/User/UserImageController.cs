df9da473f53252dc40dd8d2d5675e8a974a69e14
Senails <seffele@gmail.com>
2023-02-25 22:33:12 +0300

    work on visual

---
+++ CoinsProject/Assets/Components/Game/User/UserImageController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserImageController : MonoBehaviour
+{
+    int activeImage = 0;
+    void Update()
+    {
+        
+    }
+
+    int findActiveImage(){
+        
+
+
+
+        return 0;
+    }
+}
