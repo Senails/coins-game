b46f7fd0810369215078688ea3f7b10147909605
Senails <seffele@gmail.com>
2023-03-09 20:15:28 +0300

    debug resize camera

--- CoinsProject/Assets/Scripts/Camera/CameraScript.cs
+++ CoinsProject/Assets/Scripts/Camera/CameraScript.cs
@@ -16,10 +16,13 @@ public class CameraScript : MonoBehaviour
     float maxCameraY; 
     
     float StartCameraSize;
+    float activeCameraSize;
 
     static CameraScript Self;
 
     bool CameraMoved=false;
+
+
 
     void Start()
     {   
@@ -28,41 +31,14 @@ public class CameraScript : MonoBehaviour
 
         Self = this;
     }
-
     void Update()
     {
         CameraMoved=false;
 
-        float mw = Input.GetAxis("Mouse ScrollWheel");
-        if (Mathf.Abs(mw)>0){
-            if (mw<0){
-                changeCameraSize("more");
-            }else{
-                changeCameraSize("less");
-            }
-
-            findMaxMinCameraPosition();
-            moveCamera();
-        }
-
+        resizeCameraHandler();
         moveCamera();
     }
 
-    void findMaxMinCameraPosition(){
-        Camera camera = Camera.main;
-
-        float heightCamera = camera.orthographicSize*2;
-        float widthCamera = camera.aspect*heightCamera;
-
-        float mapWidth = GameMap.transform.localScale.x;
-        float mapHeight = GameMap.transform.localScale.y;
-
-        this.minCameraX = (widthCamera-mapWidth)/2+GameMap.transform.position.x;
-        this.maxCameraX = -((widthCamera-mapWidth)/2-GameMap.transform.position.x);
-
-        this.minCameraY = (heightCamera-mapHeight)/2+GameMap.transform.position.y;
-        this.maxCameraY = -((heightCamera-mapHeight)/2-GameMap.transform.position.y);
-    }
 
     void moveCamera(){
         if (CameraMoved) return;
@@ -85,10 +61,11 @@ public class CameraScript : MonoBehaviour
         float needCameraX = xCamera;
         float needCameraY = yCamera;
 
+        
         if (Mathf.Abs(deltaX) > widthCamera/rigidityPosition){
             needCameraX = (deltaX>0)?
-            (xUser-widthCamera/(rigidityPosition)):
-            (xUser+widthCamera/(rigidityPosition));
+            (xUser-widthCamera/rigidityPosition):
+            (xUser+widthCamera/rigidityPosition);
         }
 
         if (Mathf.Abs(deltaY) > heightCamera/rigidityPosition){
@@ -97,46 +74,87 @@ public class CameraScript : MonoBehaviour
             (yUser+heightCamera/rigidityPosition);
         }
 
+
+        needCameraX = xCamera + (needCameraX-xCamera)/(1000/rigiditySpeed);
+        needCameraY = yCamera + (needCameraY-yCamera)/(1000/rigiditySpeed);
+
+
         needCameraX = 
-        (needCameraX>=maxCameraX)?maxCameraX:
-        (needCameraX<=minCameraX)?minCameraX:needCameraX;
+        (needCameraX>maxCameraX)?maxCameraX:
+        (needCameraX<minCameraX)?minCameraX:needCameraX;
+
 
         needCameraY = 
-        (needCameraY>=maxCameraY)?maxCameraY:
-        (needCameraY<=minCameraY)?minCameraY:needCameraY;
+        (needCameraY>maxCameraY)?maxCameraY:
+        (needCameraY<minCameraY)?minCameraY:needCameraY;
 
-        float calculateX = xCamera + (needCameraX-xCamera)/(1000/rigiditySpeed);
-        float calculateY = yCamera + (needCameraY-yCamera)/(1000/rigiditySpeed);
 
-        Camera.main.transform.position= new Vector3(calculateX,calculateY,-10);
+        Camera.main.transform.position= new Vector3(needCameraX,needCameraY,-10);
     }
+    void resizeCameraHandler(){
+        float mw = Input.GetAxis("Mouse ScrollWheel");
+        if (Mathf.Abs(mw)>0){
+            if (mw<0){
+                changeActiveCameraSize("more");
+            }else{
+                changeActiveCameraSize("less");
+            }
+        }
+        changeRealCameraSize();
+    }
+
 
     void getStartCameraSize(){
         var camera = transform.GetComponent<Camera>();
-        StartCameraSize = camera.orthographicSize;
-    }
 
-    void changeCameraSize(string MoreLess){
-        var camera = transform.GetComponent<Camera>();
-        float size = camera.orthographicSize;;
+        StartCameraSize = camera.orthographicSize;
+        activeCameraSize = camera.orthographicSize;
+    }
+    void findMaxMinCameraPosition(){
+        Camera camera = Camera.main;
+
+        float heightCamera = camera.orthographicSize*2;
+        float widthCamera = camera.aspect*heightCamera;
+
+        float mapWidth = GameMap.transform.localScale.x;
+        float mapHeight = GameMap.transform.localScale.y;
+
+        this.minCameraX = (widthCamera-mapWidth)/2+GameMap.transform.position.x;
+        this.maxCameraX = -((widthCamera-mapWidth)/2-GameMap.transform.position.x);
+
+        this.minCameraY = (heightCamera-mapHeight)/2+GameMap.transform.position.y;
+        this.maxCameraY = -((heightCamera-mapHeight)/2-GameMap.transform.position.y);
+    }
+    void changeActiveCameraSize(string MoreLess){
+        float size = this.activeCameraSize;
 
         if (MoreLess == "less"){
-            size= size*.95f;
+            size= size*0.90f;
         }else{
-            size= size*1.05f;
+            size= size*1.10f;
         }
 
-        if (size>this.StartCameraSize*2){
-            size=this.StartCameraSize*2;
+        if (size>this.StartCameraSize*1.5f){
+            size=this.StartCameraSize*1.5f;
         }
         if (size<this.StartCameraSize/2){
             size=this.StartCameraSize/2;
         }
 
-        camera.orthographicSize=size;
+        this.activeCameraSize=size;
+    }
+    void changeRealCameraSize(){
+        var camera = transform.GetComponent<Camera>();
+        float size = camera.orthographicSize;
+
+        if (Mathf.Abs(this.activeCameraSize-size)>0.001){
+            size=size+(this.activeCameraSize-size)/((1/Time.deltaTime)/10);
+
+
+            camera.orthographicSize=size;
+            findMaxMinCameraPosition();
+        }
+
     }
 
-    static public void MoveCamera(){
-        CameraScript.Self.moveCamera();
-    }
 }
